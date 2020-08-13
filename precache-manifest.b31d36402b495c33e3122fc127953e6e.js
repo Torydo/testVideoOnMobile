271e9478582f6b7378192b5ab6ccdbf0474d519f
verint torydo <torydo@example.com>
2020-08-13 10:18:11 -0300

    initail commit

---
+++ precache-manifest.b31d36402b495c33e3122fc127953e6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da604c8181b8cb2935e2e0e38174be5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54831c64e074b02dc655",
+    "url": "/static/css/main.dbcc34e1.chunk.css"
+  },
+  {
+    "revision": "e9f7aba61547f294e629",
+    "url": "/static/js/2.5cd7348e.chunk.js"
+  },
+  {
+    "revision": "54831c64e074b02dc655",
+    "url": "/static/js/main.18e1b6ea.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "75677d54d8fae2678539a58e685a9d23",
+    "url": "/static/media/apple.75677d54.svg"
+  }
+]);
