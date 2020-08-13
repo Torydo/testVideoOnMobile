d54b2920f6d0a1802c4096e439b2b74554d129ec
verint torydo <torydo@example.com>
2020-08-13 12:14:36 -0300

    upgrade v1.2

---
+++ precache-manifest.47cf5943d93ed308842133c33a70f01b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80fb727973c23a8715c5b23fc815e9f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "161f2d9ed98c4901f65a",
+    "url": "/static/css/main.dbcc34e1.chunk.css"
+  },
+  {
+    "revision": "f0b521c66bc702a37631",
+    "url": "/static/js/2.9f4f2337.chunk.js"
+  },
+  {
+    "revision": "161f2d9ed98c4901f65a",
+    "url": "/static/js/main.a7d6c9a2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "75677d54d8fae2678539a58e685a9d23",
+    "url": "/static/media/apple.75677d54.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aa7cb62b5784c4bec87620f6c375bb17.js"
+  "/precache-manifest.47cf5943d93ed308842133c33a70f01b.js"
 );
 
 self.addEventListener('message', (event) => {
